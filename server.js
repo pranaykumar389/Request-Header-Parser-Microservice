7efbb2a48d4dd0791f20296404c98c6155f0022c
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2016-12-10 17:45:37 -0500

    Added requestip package and route.

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 var express = require('express'),
     app = express(),
-    requestIp = require(request-ip);
+    requestIp = require('request-ip');
 
 app.get('/', function(req, res) {
     res.sendFile(process.cwd() + '/index.html');
 });
 
+//
 app.get('/api/findip', function(req, res) {
-    var ip = req.ip;
-         
+    // req.ip
+    var ip = requestIp.getClientIp(req),
+        lang = req.headers['accept-language'].split(',')[0],
+        software = req.headers['user-agent'].split(') ')[0].split(' (')[1];
+    
+    res.json({
+        "ipaddress": ip,
+        "language": lang,
+        "software": software
+    });
+});
+
+app.listen(8080, function() {
+    console.log('Server Listening on port 8080!');
 });
