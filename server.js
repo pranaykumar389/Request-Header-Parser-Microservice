f5d52679ec401d409d1aad0c9c0ed53aaedfad9b
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2016-12-10 17:28:21 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express'),
+    app = express(),
+    requestIp = require(request-ip);
+
+app.get('/', function(req, res) {
+    res.sendFile(process.cwd() + '/index.html');
+});
+
+app.get('/api/findip', function(req, res) {
+    var ip = req.ip;
+    
+});
